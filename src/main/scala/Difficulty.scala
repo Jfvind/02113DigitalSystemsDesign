c8c4cfe39d761a4cd335499f0a2769c9f9036df1
freddyTheAlch <s220768@dtu.dk>
2025-06-23 12:03:36 +0200

    Fix Diff

--- src/main/scala/Difficulty.scala
+++ src/main/scala/Difficulty.scala
@@ -10,8 +10,6 @@ class Difficulty extends Module {
     val score = Output(UInt(16.W))
   })
 
-  val x = RegInit(1.S(7.W))
-  val xDone = RegInit(false.B)
   val speedCnt = RegInit(0.U(27.W))
 
   when(io.resetSpeed) {
@@ -20,31 +18,34 @@ class Difficulty extends Module {
     speedCnt := speedCnt + 1.U
   }
 
-  val timeInSeconds = speedCnt >> 6 // ≈ /64, giver os ca 1 inkremering i sek
-  val scaledTime = timeInSeconds >> 2 // ≈ time / 4
+  def saturatingSub(a: UInt, b: UInt): UInt = Mux(a > b, a - b, 0.U) //Vigtig for underflow
 
-  val scoreBase = MuxLookup(io.level, 1.U)(Seq(
-    1.U -> 1.U,
-    2.U -> 2.U,
-    3.U -> 3.U
-  ))
-
-  io.score := timeInSeconds * scoreBase
+  val timeInSeconds = speedCnt >> 6 // ca. 1/s
+  val scaledTime = timeInSeconds >> 2
 
   val rawSpeed = MuxLookup(io.level, 5.S)(Seq(
     1.U -> (2.S + scaledTime.asSInt),
     2.U -> (3.S + scaledTime.asSInt * 2.S),
     3.U -> (5.S + scaledTime.asSInt * 3.S)
   ))
-
-  io.speed := Mux(rawSpeed > 10.S, 10.S, rawSpeed)
-
-  val spawnInterval = MuxLookup(io.level, 30.U)(Seq(
-    1.U -> (80.U - timeInSeconds),
-    2.U -> (60.U - timeInSeconds * 2.U),
-    3.U -> (40.U - timeInSeconds * 3.U)
+  val speedCap = MuxLookup(io.level, 10.S)(Seq(
+    1.U -> 8.S,
+    2.U -> 12.S,
+    3.U -> 16.S
   ))
-  io.spawnInterval := Mux(spawnInterval < 10.U, 10.U, spawnInterval)
+  io.speed := Mux(rawSpeed > speedCap, speedCap, rawSpeed)
 
-  //Husk Liv
+  val rawSpawn = MuxLookup(io.level, 30.U)(Seq(
+    1.U -> saturatingSub(80.U, timeInSeconds),
+    2.U -> saturatingSub(60.U, timeInSeconds * 2.U),
+    3.U -> saturatingSub(40.U, timeInSeconds * 3.U)
+  ))
+  io.spawnInterval := Mux(rawSpawn < 10.U, 10.U, rawSpawn)
+
+  val scoreMultiplier = MuxLookup(io.level, 1.U)(Seq(
+    1.U -> 1.U,
+    2.U -> 3.U,
+    3.U -> 5.U
+  ))
+  io.score := timeInSeconds * scoreMultiplier
 }
