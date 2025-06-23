c417fb814c30589dc14c26bd40a25b150bd807e8
freddyTheAlch <s220768@dtu.dk>
2025-06-23 11:50:25 +0200

    nyeste ændring i diff

--- src/main/scala/Difficulty.scala
+++ src/main/scala/Difficulty.scala
@@ -21,6 +21,7 @@ class Difficulty extends Module {
   }
 
   val timeInSeconds = speedCnt >> 6 // ≈ /64, giver os ca 1 inkremering i sek
+  val scaledTime = timeInSeconds >> 2 // ≈ time / 4
 
   val scoreBase = MuxLookup(io.level, 1.U)(Seq(
     1.U -> 1.U,
@@ -31,18 +32,19 @@ class Difficulty extends Module {
   io.score := timeInSeconds * scoreBase
 
   val rawSpeed = MuxLookup(io.level, 5.S)(Seq(
-    1.U -> (3.S + timeInSeconds.asSInt * 1.S),
-    2.U -> (5.S + timeInSeconds.asSInt * 2.S),
-    3.U -> (7.S + timeInSeconds.asSInt * 3.S)
+    1.U -> (2.S + scaledTime.asSInt),
+    2.U -> (3.S + scaledTime.asSInt * 2.S),
+    3.U -> (5.S + scaledTime.asSInt * 3.S)
   ))
 
-  io.speed := Mux(rawSpeed > 20.S, 20.S, rawSpeed)
+  io.speed := Mux(rawSpeed > 10.S, 10.S, rawSpeed)
 
-  io.spawnInterval := MuxLookup(io.level, 30.U) (Seq(
-    1.U -> 60.U,
-    2.U -> 40.U,
-    3.U -> 15.U
+  val spawnInterval = MuxLookup(io.level, 30.U)(Seq(
+    1.U -> (80.U - timeInSeconds),
+    2.U -> (60.U - timeInSeconds * 2.U),
+    3.U -> (40.U - timeInSeconds * 3.U)
   ))
+  io.spawnInterval := Mux(spawnInterval < 10.U, 10.U, spawnInterval)
 
   //Husk Liv
 }
