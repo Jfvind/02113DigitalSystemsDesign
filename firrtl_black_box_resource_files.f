d308bb57e8038e9a842e7bdbe20f87731d36f83b
Nikolai <s245999@dtu.dk>
2025-06-24 15:22:16 +0200

    ændringer

--- firrtl_black_box_resource_files.f
+++ firrtl_black_box_resource_files.f
@@ -1,2 +1,2 @@
-C:\DTU-local\Repos\02113DigitalSystemsDesign\RamInitSpWf.v
-C:\DTU-local\Repos\02113DigitalSystemsDesign\RamSpWf.v
+C:\Users\nikol\Desktop\02113DigitalSystemsDesign\RamInitSpWf.v
+C:\Users\nikol\Desktop\02113DigitalSystemsDesign\RamSpWf.v
