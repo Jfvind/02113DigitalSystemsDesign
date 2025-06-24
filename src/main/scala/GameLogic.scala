03d3085e5bdbeebb33008e07fd6042e344f20fa1
Jfvind <s245326@dtu.dk>
2025-06-24 19:07:53 +0200

    fix backtiles

--- src/main/scala/GameLogic.scala
+++ src/main/scala/GameLogic.scala
@@ -319,9 +319,9 @@ class GameLogic(SpriteNumber: Int, BackTileNumber: Int, TuneNumber: Int) extends
   ))
 
   def hsRestoreTile(level: UInt): UInt = MuxLookup(level, 10.U)(Seq(
-    1.U -> 10.U,
-    2.U -> 21.U,
-    3.U -> 29.U
+    1.U -> 11.U,
+    2.U -> 23.U,
+    3.U -> 31.U
   ))
 
 
